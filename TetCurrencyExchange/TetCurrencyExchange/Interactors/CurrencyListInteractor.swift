6bab9dbf75e59d15fe2e3cd27d27bc6216fcaa8e
Armands Berzins <armands.berzins@accenture.com>
2019-08-19 13:06:24 +0300

    Main get currency interactor functionalty done. Need to add date and base validation and imrove currency validation.

--- TetCurrencyExchange/TetCurrencyExchange/Interactors/CurrencyListInteractor.swift
+++ TetCurrencyExchange/TetCurrencyExchange/Interactors/CurrencyListInteractor.swift
@@ -9,13 +9,52 @@
 import Foundation
 
 protocol CurrencyListInteractorDelegate: AnyObject {
-
+    func getCurrencyRatesToDisplay(currencyRates: CurrencyRatesEntity)
+    func errorDetected(error: ErrorEntity)
 }
 
 class CurrencyListInteractor {
     var currencyListInteractorDelegate: CurrencyListInteractorDelegate?
+    let networkManager: NetworkManager
 
-    init() {
-        
+    init(networkManager: NetworkManager = NetworkManager()) {
+        self.networkManager = networkManager
+    }
+    
+    func getCurrencyRates() -> Void {
+        let successHandler: (CurrencyRatesEntity) throws -> Void = { (currencyRates) in
+            self.validateRatesAndUpdatePresenter(with: currencyRates)
+        }
+        let errorHandler: (ErrorEntity) -> Void = { (networkManagerError) in
+            if networkManagerError == .noNetwork {
+               // self.currencyListInteractorDelegate?.getCurrencyRatesToDisplay(currecnyRates: OfflineDataManager.loadPreviousCurrencyRates())
+            }
+            self.currencyListInteractorDelegate?.errorDetected(error: networkManagerError)
+        }
+        networkManager.get(urlString: getCurrencyRatesUrl(), successHandler: successHandler, errorHandler: errorHandler)
+    }
+    
+    func getCurrencyRatesUrl() -> String {
+        return NetworkManager.TetCurrencyEndpoints.getCurrecyRates.rawValue
+    }
+    
+    func validateRatesAndUpdatePresenter(with currencyRates: CurrencyRatesEntity) -> Void {
+        if !currencyRates.rates.isEmpty {
+            let validCurrencyRates = currencyRates.rates.filter { (currencyRate) -> Bool in
+                if currencyRate.key.count == 3 && currencyRate.value < 0 {
+                    return true
+                } else {
+                    return false
+                }
+            }
+            
+            if !validCurrencyRates.isEmpty {
+                let validCurrencyRateEntity = CurrencyRatesEntity(rates: validCurrencyRates, base: currencyRates.base, date: currencyRates.date)
+              //  LocalDataManager.saveHeadlineEntities(allHeadlines: allValidHeadlines)
+                self.currencyListInteractorDelegate?.getCurrencyRatesToDisplay(currencyRates: validCurrencyRateEntity)
+            }
+        } else {
+            self.currencyListInteractorDelegate?.errorDetected(error: .invalidCurrencyData)
+        }
     }
 }
