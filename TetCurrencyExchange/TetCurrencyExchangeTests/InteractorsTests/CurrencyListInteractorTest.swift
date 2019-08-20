c40df4274f0683ed568fa2bafe63ecf8292fc2e4
Armands Berzins <armands.berzins@accenture.com>
2019-08-20 20:41:46 +0300

    Offline mode test simulation added to interactor

--- TetCurrencyExchange/TetCurrencyExchangeTests/InteractorsTests/CurrencyListInteractorTest.swift
+++ TetCurrencyExchange/TetCurrencyExchangeTests/InteractorsTests/CurrencyListInteractorTest.swift
@@ -22,12 +22,16 @@ class CurrencyListInteractorTest: XCTestCase, CurrencyListInteractorDelegate {
     let fakeCorrectCorrencyRateInstance = CurrencyRatesEntity (rates: ["CAD":1.4841,"HKD":8.744,"ISK":137.9,"PHP":58.759,"DKK":7.459,"HUF":326.2,"CZK":25.876,"AUD":1.6442,"RON":4.7231,"SEK":10.7305,"IDR":15911.05,"INR":79.5205,"BRL":4.4868,"RUB":73.7771,"HRK":7.3858,"JPY":118.37,"THB":34.403,"CHF":1.0863,"SGD":1.5483,"PLN":4.3878,"BGN":1.9558,"TRY":6.2316,"CNY":7.8463,"NOK":10.0145,"NZD":1.7311,"ZAR":17.044,"USD":1.115,"MXN":21.8595,"ILS":3.9237,"GBP":0.91863,"KRW":1353.11,"MYR":4.6758], base: "EUR", date: "2019-08-15")
     
     public class FakeNetworkManager: NetworkManager {
+        public var simulateOffline = false
         public var successResponse:Codable?
         public var errorResponse: String?
         open override func get<T>(urlString: String,
                                   headers: [String : String],
                                   successHandler: @escaping (T) throws -> Void,
                                   errorHandler: @escaping ErrorHandler) where T : Decodable {
+            if simulateOffline {
+                errorHandler(.noNetwork)
+            }
             switch successResponse {
             case .some(let response):
                 if let correctResponse = response as? T {
@@ -103,4 +107,12 @@ class CurrencyListInteractorTest: XCTestCase, CurrencyListInteractorDelegate {
         waitForExpectations(timeout: 2)
         XCTAssertNil(resultCurrencyRates)
     }
+    
+    func test_Offline_getScoreData_callsGetHeadlinesToDisplay_onSuccess() {
+        fakeNetworkManager.simulateOffline = true
+        currencyListInteractor.getCurrencyRates()
+        waitForExpectations(timeout: 2)
+        XCTAssertTrue(delegateCalledSuccessfully, "should call getUserScoreToDisplay if score data was valid")
+    }
+
 }
