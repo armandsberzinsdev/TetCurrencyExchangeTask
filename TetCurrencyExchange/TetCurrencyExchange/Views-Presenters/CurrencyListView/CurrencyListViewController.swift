59b213bcccaf6cc87eaaf880d5b8a84846f7f142
Armands Berzins <armands.berzins@accenture.com>
2019-08-21 00:18:33 +0300

    main list ViewController code added with  added table view

--- TetCurrencyExchange/TetCurrencyExchange/Views-Presenters/CurrencyListView/CurrencyListViewController.swift
+++ TetCurrencyExchange/TetCurrencyExchange/Views-Presenters/CurrencyListView/CurrencyListViewController.swift
@@ -7,26 +7,67 @@
 
 import UIKit
 
-class CurrencyListViewController: UIViewController {
+class CurrencyListViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var currencyListTableView: UITableView!
 
     let uiPresenter = CurrencyListPresenter()
     weak var uiPresenterDelegate: CurrencyListPresenterDelegate?
+    var currentCurrencyRates: CurrencyRatesEntity
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViewController()
+    }
+    
+    func setupViewController() -> Void {
+        uiPresenter.uiPresenterDelegate = self
+        currencyListTableView.delegate = self
+        currencyListTableView.dataSource = self
+        currencyListTableView.register(UINib.init(nibName: "CurrecyRateCell", bundle: nil), forCellReuseIdentifier: "CurrecyRateCell")
     }
 
 
 }
 
+extension CurrencyListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currentCurrencyRates.rates.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CurrecyRateCell") as! CurrecyRateTableViewCell
+        let ratesArray = Array(currentCurrencyRates.rates)
+        ratesArray[indexPath.row]
+    //    {
+//            cell.headlineLabel.text = headline.headline
+//            cell.lastUpdatedLabel.text = prepareDate(for: headline)
+    //    }
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+//        if let selectedHeadline = self.allHeadlines?.headlines[indexPath.row] {
+//            presenter.navigateToHeadlineDetails(from: self, with: selectedHeadline)
+//        } else {
+//            presentErrorView(error: .invalidData)
+//        }
+    }
+}
+
 extension CurrencyListViewController: CurrencyListPresenterDelegate {
     func updateCurrencyListData(with currencyRates: CurrencyRatesEntity) {
-        print("aaa")
+        self.currentCurrencyRates = currencyRates
+        self.currencyListTableView.reloadData()
     }
     
     func presentErrorView(error: ErrorEntity) {
-        print("bbb")
+        error.alert(with: self)
     }
     
 
